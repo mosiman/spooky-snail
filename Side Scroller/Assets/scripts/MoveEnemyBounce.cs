97af009455b0118da739eb194a823fa99f4dc1af
Andre Ha <andre.oikawa@hotmail.com>
2017-01-21 18:32:19 -0500

    Bouncing fish

---
+++ Side Scroller/Assets/scripts/MoveEnemyBounce.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemyBounce : MonoBehaviour {
+
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        this.transform.position = new Vector3 (this.transform.position.x + 5, 0, 0);
+        DeleteInstance();
+    }
+
+    void RandomSpawn(float min, float max)
+    {
+        
+    }
+
+    void DeleteInstance()
+    {
+        Vector3 position = Camera.main.WorldToScreenPoint(this.transform.position);
+        print(position);
+        if (position.x < 0 || position.y < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Side Scroller/Assets/scripts/MoveFloor.cs
+++ Side Scroller/Assets/scripts/MoveFloor.cs
@@ -4,37 +4,41 @@ using UnityEngine;
 
 public class MoveFloor : MonoBehaviour
 {
+    public Transform enemy_1;
 
-    public float speed = 1f;
-    Vector3 pos;
-
+    public Vector3 speed = new Vector3(1,0,0);
+    BoxCollider2D rb;
+    Vector3 worldPosition;
+    Vector3 rbOffsetAndSize;
     // Use this for initialization
     void Start()
     {
-        pos = this.transform.position;
+        Vector3 pos = this.transform.position;
+        rb = GetComponent<BoxCollider2D>();
+        rbOffsetAndSize = new Vector3 (rb.offset.x + rb.size.x/2,0,0);
+        worldPosition = Camera.main.WorldToScreenPoint(new Vector3(pos.x + rbOffsetAndSize.x, pos.y, 0));
+        //print("worldPos:" + worldPosition.x.ToString() + " screen-world: " + (Camera.main.ScreenToWorldPoint(worldPosition) - rbOffsetAndSize).ToString()
+        //    + " position: " + this.transform.position.ToString() + " speed: " + speed.ToString());
     }
 
     // Update is called once per frame
     void Update()
     {
-        pos.x -= speed;
-        this.transform.position = pos;
-        //print(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        worldPosition.x -= speed.x;
+        //print("worldPos:" + worldPosition.x.ToString() + " screen-world: " + (Camera.main.ScreenToWorldPoint(worldPosition) - rbOffsetAndSize).ToString()
+        //    + " position: " + this.transform.position.ToString() + " speed: " + speed.ToString());
+        this.transform.position = Camera.main.ScreenToWorldPoint(worldPosition) - rbOffsetAndSize;
         Loop();
     }
 
     void Loop()
     {
-
-        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(this.transform.position);
-        print(worldPosition.ToString());
-        worldPosition.z = 0;
-        //Debug.Log ("World Position: " + worldPosition.ToString());
-        if (pos.x < 0)
+        if (worldPosition.x < 0)
         {
- //           pos.x += Camera.main.pixelWidth;
+            worldPosition.x = Camera.main.pixelWidth;
+            worldPosition = Camera.main.WorldToScreenPoint((Camera.main.ScreenToWorldPoint(worldPosition) + new Vector3(rb.size.x, 0, 0)));
+            Instantiate(enemy_1, new Vector3(2.471426f, -0.1496139f, 0), Quaternion.identity);
         }
-   //     this.transform.position = worldPosition;
 
     }
 }
