f01568a7a4d8ba09db68e695a6083ac8c33588a6
mosiman <dillonchan97@yahoo.com>
2017-01-21 13:32:13 -0500

    basic mainchar movement

---
+++ Side Scroller/Assets/mainChar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainChar : MonoBehaviour {
+    
+    double g = -9.8;
+    Vector3 startPos;
+    public float jumpVel = 5f;
+
+
+	// Use this for initialization
+	void Start () {
+        print("hello from char script!");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Pressed 'W', jump
+	    if (Input.GetKeyDown(KeyCode.W)){
+                Jump();
+                print("jump!");
+        }
+
+	}
+
+    void Jump() {
+        print(this.transform.position);
+        this.GetComponent<Rigidbody2D>().addForce(jumpVel * Vector3.up);
+
+    }
+}
