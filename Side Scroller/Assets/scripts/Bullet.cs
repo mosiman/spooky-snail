4c9a9cc4b075e9403e8af3d50bba835d0913d517
Leo Pan <leo.pan.duh@gmail.com>
2017-01-22 16:12:33 -0500

    add assets back

---
+++ Side Scroller/Assets/scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public Vector3 speed = new Vector3(8, 0, 0);
+    Vector3 relativePos;
+    CapsuleCollider2D rb;
+    Vector3 pos;
+
+    // Use this for initialization
+    void Start ()
+    {
+        pos = this.transform.position;
+        rb = GetComponent<CapsuleCollider2D>();
+        relativePos = Camera.main.WorldToScreenPoint(new Vector3(pos.x - rb.offset.x, pos.y, 0));
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        pos = this.transform.position;
+        relativePos.x += speed.x*mainChar.slowmotion;
+        this.transform.position = Camera.main.ScreenToWorldPoint(relativePos);
+        if (pos.x > 3.35)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
