47193071a1ead82fa785cea0c5ecba03d812b181
Leo Pan <leo.pan.duh@gmail.com>
2017-01-21 20:19:46 -0500

    add bullet fire and bullet spawn + despawn

--- Side Scroller/Assets/scripts/Bullet.cs
+++ Side Scroller/Assets/scripts/Bullet.cs
@@ -7,11 +7,12 @@ public class Bullet : MonoBehaviour {
     public Vector3 speed = new Vector3(8, 0, 0);
     Vector3 relativePos;
     CapsuleCollider2D rb;
+    Vector3 pos;
 
     // Use this for initialization
     void Start ()
     {
-        Vector3 pos = this.transform.position;
+        pos = this.transform.position;
         rb = GetComponent<CapsuleCollider2D>();
         relativePos = Camera.main.WorldToScreenPoint(new Vector3(pos.x - rb.offset.x, pos.y, 0));
 
@@ -19,7 +20,12 @@ public class Bullet : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        pos = this.transform.position;
         relativePos.x += speed.x;
         this.transform.position = Camera.main.ScreenToWorldPoint(relativePos);
+        if (pos.x > 3.35)
+        {
+            Destroy(gameObject);
+        }
 	}
 }
