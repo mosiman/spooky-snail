ab926872978030b58a285e4b612cc7098cb88b50
Andre Ha <andre.oikawa@hotmail.com>
2017-01-22 14:57:11 -0500

    Enemy2

---
+++ Side Scroller/Assets/scripts/EnemyMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+    float camOriginX = -3.3428f;
+    float camOriginY = -1.5f;
+    //float camRightmost = 3.3428f;
+
+    public Transform explosion;
+
+    CircleCollider2D circle;
+    float size;
+    public float speedConst = 1.5f;
+    // Use this for initialization
+    void Start () {
+        circle = GetComponent<CircleCollider2D>();
+        size = circle.radius;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(Vector3.left * Time.deltaTime * speedConst *
+                                 background.speedupConst*mainChar.slowmotion);
+
+        if ((this.transform.position.x + size) < camOriginX || this.transform.position.y + size < camOriginY)
+        {
+                Destroy(gameObject);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.transform.gameObject.name == "Pellet(Clone)")
+        {
+            Instantiate(explosion, this.transform.position, Quaternion.identity);
+            Destroy(coll.transform.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
