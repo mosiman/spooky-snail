3bb0621989b20a981f1d16387d6845e3364382c1
Leo Pan <leo.pan.duh@gmail.com>
2017-01-21 21:19:46 -0500

    fix corruptions

--- Side Scroller/Assets/scripts/MoveFloor.cs
+++ Side Scroller/Assets/scripts/MoveFloor.cs
@@ -4,14 +4,8 @@ using UnityEngine;
 
 public class MoveFloor : MonoBehaviour
 {
-<<<<<<< HEAD
-    public Transform enemy_1;
-
-    public Vector3 speed = new Vector3(1,0,0);
-=======
 
     public Vector3 speed = new Vector3(1, 0, 0);
->>>>>>> 622696af15186dfc175f6cd7f3141d6a17b1aefe
     BoxCollider2D rb;
     Vector3 worldPosition;
     Vector3 rbOffsetAndSize;
@@ -20,11 +14,7 @@ public class MoveFloor : MonoBehaviour
     {
         Vector3 pos = this.transform.position;
         rb = GetComponent<BoxCollider2D>();
-<<<<<<< HEAD
-        rbOffsetAndSize = new Vector3 (rb.offset.x + rb.size.x/2,0,0);
-=======
         rbOffsetAndSize = new Vector3(rb.offset.x + rb.size.x / 2, 0, 0);
->>>>>>> 622696af15186dfc175f6cd7f3141d6a17b1aefe
         worldPosition = Camera.main.WorldToScreenPoint(new Vector3(pos.x + rbOffsetAndSize.x, pos.y, 0));
         //print("worldPos:" + worldPosition.x.ToString() + " screen-world: " + (Camera.main.ScreenToWorldPoint(worldPosition) - rbOffsetAndSize).ToString()
         //    + " position: " + this.transform.position.ToString() + " speed: " + speed.ToString());
@@ -46,11 +36,6 @@ public class MoveFloor : MonoBehaviour
         {
             worldPosition.x = Camera.main.pixelWidth;
             worldPosition = Camera.main.WorldToScreenPoint((Camera.main.ScreenToWorldPoint(worldPosition) + new Vector3(rb.size.x, 0, 0)));
-<<<<<<< HEAD
-            //Vector3 speed1 = Camera.main.WorldToScreenPoint(speed);
-            Instantiate(enemy_1, new Vector3(2.471426f, -0.1496139f, 0), Quaternion.identity);
-=======
->>>>>>> 622696af15186dfc175f6cd7f3141d6a17b1aefe
         }
 
     }
